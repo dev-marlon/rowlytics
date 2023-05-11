1e7eb7e97f452d7f3a0deea1850eb344be1b15a0
dev-marlon <info@t3labs.de>
2023-05-11 18:48:14 +0200

    Different students data can now be selected

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { computed, defineProps, ref } from 'vue'
 
 interface Props {
     total: number
@@ -7,21 +7,22 @@ interface Props {
 }
 const props = defineProps<Props>()
 
-const value = (props.total / props.max) * 100
+const value = computed(() => (props.total / props.max) * 100)
 
-const color = (value) => {
-    if (value > 75 && value < 100) {
+const color = ref((value) => {
+    console.log(value)
+    if (value >= 75 && value <= 100) {
         return 'green'
-    } else if (value > 50 && value < 75) {
+    } else if (value >= 50 && value < 75) {
         return 'lime'
-    } else if (value > 25 && value < 50) {
+    } else if (value >= 25 && value < 50) {
         return 'orange'
-    } else if (value > 0 && value < 25) {
+    } else if (value >= 0 && value < 25) {
         return 'red'
     } else {
         return '#000'
     }
-}
+})
 </script>
 <template>
     <v-progress-linear
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,454 +1,9 @@
 <script setup lang="ts">
 import ProgressBar from '@/components/ProgressBar.vue'
 import { TechnicalSkill, TechnicalSkillItem } from '@/models/technical-skill.model'
-
-const technicalSkills: TechnicalSkill[] = [
-    {
-        name: 'Recovery',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Sauberer <strong>Trennung</strong> des Vorrollens (Arme-Oberkörper-Rolle)',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 2,
-                description: '<strong>Lange Arme.</strong>',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 3,
-                description:
-                    'Die Oberkörpervorlage (65°) wird vor dem Vorrollen erreicht, das Gewicht verlagert sich in Richtung der Füße <strong>(RTK)</strong>. ("Twerking")',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 4,
-                description: 'Slide control. <strong>On the Spot</strong>',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 5,
-                description:
-                    'Der Drehimpuls kommt aus den Fingergelenken, nicht dem Handgelenk ("<strong>Tiger fist</strong>")',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 6,
-                description:
-                    'Die Hände haben vor und nach der Umkehr die gleiche Geschwindigkeit ("<strong>Boomerang</strong>")',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 7,
-                description: '<strong>Handführung</strong> (Tap down / Helium)',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 8,
-                description:
-                    '<strong>Richtiges Sitzen</strong> auf dem Rollsitz (Auf den Sitzknochen, nicht dem Steißbein) (RTK)',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 9,
-                description:
-                    'Die Blätter werden freiwasser nach vorne geführt (<strong>nicht schleifen</strong>)',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description:
-                    'Oberkörperstabilität in der Rücklage. (Breite Schultern, das Boot laufen lassen)',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description:
-                    '<strong>Reach out</strong>. Breite Schultern. <strong>Flat "V"</strong>',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description: '<strong>Dynamic, Boat fluctuations.</strong>',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description: '<strong>Früh aufdrehen</strong>, ab RTK',
-                score: 1,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Catch',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Die Blätter werden <strong>direkt, schnell und sauber</strong> gesetzt',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 2,
-                description: 'Die Schienenbeine sind vertikal im Moment des Setzens',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 3,
-                description:
-                    '<strong>"V" Catch</strong>. Es wird leicht gegen die Fahrtrichtung gesetzt, die blätter sind im Wasser bevor der Durchzug beginnt ("Back-Splash")',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 4,
-                description: '<strong>Reach out</strong>. Breite Schultern. Flat "V"',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 5,
-                description:
-                    'Es gibt in der Auslage keine Pause, direkter Übergang vom Vorrollen in den Durchzug. <strong>Elastic collision. Bauncing. Basket ball.</strong>',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 6,
-                description:
-                    'Die Blätter sind gesetzt, wenn die maximale Auslage mit der Rolle erreicht wird (<strong>Catch mit der Rolle</strong>). ',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 7,
-                description:
-                    'Die Setzbewegung erfolgt aus dem Schultergelenk und über die Hände, nicht aus dem Oberkörper. ',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 8,
-                description: 'Der Oberkörper wird beim Setzen nicht abgesenkt',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 9,
-                description:
-                    'Die Auslage wird nicht überstreckt (Kein weiteres Vorgehen mit dem Oberkörper in der Auslage)',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description: 'Kein Wegrutschen',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 11,
-                description:
-                    'Das Boot macht beim Setzen kein Pause, kontinuierliche Geschwindigkeit. ',
-                score: 2,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Drive',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Der Oberkörper wird erst geöffnet wenn die Beine den Zug beendet haben, RTK',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 2,
-                description: '<strong>Oberkörperschwung</strong>',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 3,
-                description:
-                    'Die Blätter werden im Wasser früh verankert, sodass die Beine vollständig genutzt werden können',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 4,
-                description: 'Hängen an den Griffen, das Gewicht wird leicht vom Rollsitz genommen',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 5,
-                description:
-                    'Korrekte Reihenfolge der Bewegungen erst die Beine, dann der Oberkörper und dann die Arme. Ein langer und kontrollierter Schlag',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 6,
-                description: 'Horizontaler Durchzug im ganzen Schlag',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 7,
-                description:
-                    'Druckverteilung über den ganzen Durchzug, kein "Reinknallen"/"Hammerschlag"',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 8,
-                description:
-                    'Die Ellenbogen bleiben oberhalb der Griffe und zeigen nach außen, nicht nach unten',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 9,
-                description:
-                    'Die Blätter werden im Wasser nicht versenkt, Führung knapp unterhalb der Wasseroberfläche',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description: 'Es wird die Richtung des Sitzes nicht der Griffe geändert',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 11,
-                description: '<strong>Trennung von Endzug und Aushebeln</strong>',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 12,
-                description:
-                    '<strong>Der Kontakt mit dem Stemmbrett wird in der hinteren Umkehr nicht verloren</strong>',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 13,
-                description:
-                    'Die Hände hängen an den Griffen, die Arme bleiben so lange wie möglich gestreckt. Hand = Huck / Arm = Rope',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 14,
-                description:
-                    'Sauberes Aushebeln nach dem Endzug, die Handgelenke bleiben gerade, Abdrehen aus den Fingern',
-                score: 2,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Posture',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Horizontale Haltung. Arme, Schultern und Blickrichtung bleiben im Schlag horizontal',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 2,
-                description: 'Seitliche Drehung der Ellenbogen',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 3,
-                description:
-                    'Das Körpergewicht wird oben gehalten, kein Zusammensacken in der Auslage',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 4,
-                description:
-                    'Das Körpergewicht wird oben gehalten, kein Zusammensacken in der Rücklage',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 5,
-                description: 'Handgelenke gerade ',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 6,
-                description: 'Oberkörperstabilität in der Rücklage. (Das Boot laufen lassen) ',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 7,
-                description: 'Kopf bleibt auf die selbe Ebene. Freilauf/ Drive',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 8,
-                description: 'Gute RTK Position in der Freilauf erreicht ',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 9,
-                description: 'Die Oberkörperrotation kommt aus dem Becken, dieses wird rotiert',
-                score: 3,
-                maximalScore: 3
-            },
-            {
-                index: 10,
-                description: 'Vertikale Schienenbeine in der Auslage und beim Setzen',
-                score: 3,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Blade work',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Die Blätter werden erst auf Höhe der Dollen geführt, dann in der Setzbewegung abgesenkt (<strong>Tap down / Helium</strong>)',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 2,
-                description:
-                    'Die Blätter werden erst am Ende des Durchzuges aus dem Wasser gehebelt. <strong>No Wash-out</strong>',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 3,
-                description: 'Die Blätter werden direkt, schnell und sauber gesetzt',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 4,
-                description: 'Früh aufdrehen. Ab RTK',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 5,
-                description: 'Horizontal Drive. Blättern auf horizontaler Ebene.',
-                score: 3,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Grip',
-        items: [
-            {
-                index: 1,
-                description: 'Hängen an den Griffen, lockere Hände',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description: 'Die Griffe liegen in den Fingern, nicht der Handinnenfläche',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description:
-                    'Im Vorrollen ist der Druck an den Griffen gelöst, Entspannung in den Händen',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description: 'Die Griffe werden in die Hände Eingedreht',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description: 'Die Unterarme bleiben entspannt',
-                score: 2,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description:
-                    'Die Hände hängen an den Griffen, die Arme bleiben so lange wie möglich gestreckt',
-                score: 1,
-                maximalScore: 3
-            },
-            {
-                index: 1,
-                description: 'Enge Führung am Ende (?)',
-                score: null,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Timing - Boat Velocity',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Gutes Gefühl für den Bootsrythmus und die Bootsgeschwindigkeit. Es gelingt ein gutes Timing für das setzen und aushebeln mit dem Rest des Bootes',
-                score: null,
-                maximalScore: 3
-            }
-        ]
-    },
-    {
-        name: 'Flexibility',
-        items: [
-            {
-                index: 1,
-                description:
-                    'Ausreichende Flexibilität im Knöchel, Oberschenkelrückseite und Hüfte um eine gute Position zu halten. ',
-                score: 2,
-                maximalScore: 3
-            }
-        ]
-    }
-]
+import { technicalSkills as technicalSkillsJohannes } from '@/data/johannes'
+import { technicalSkills } from '@/data/marie'
+import { computed, reactive, ref } from 'vue'
 
 const total = (technicalSkill: TechnicalSkill) => {
     return accumulateValue(technicalSkill.items, 'score')
@@ -477,14 +32,38 @@ const accumulateValue = (items: TechnicalSkillItem[], key: string): number => {
             .reduce((previousValue, currentValue) => (previousValue || 0) + (currentValue || 0), 0)
     )
 }
+
+const dataMap = {
+    marie: technicalSkills,
+    johannes: technicalSkillsJohannes
+}
+
+const selected = ref({
+    title: 'Bitte wählen',
+    value: ''
+})
+
+const selectItems = [
+    { title: 'Marie', value: 'marie' },
+    { title: 'Johannes', value: 'johannes' }
+]
+
+const items = computed(() => {
+    return dataMap[selected.value]
+})
 </script>
 
 <template>
     <v-container>
         <h2 class="pagetitle">Technical goal</h2>
-
+        <v-select
+            v-model="selected"
+            :items="selectItems"
+            label="Student"
+            density="compact"
+        ></v-select>
         <v-expansion-panels variant="accordion" :multiple="true" class="technical-skills-panels">
-            <v-expansion-panel v-for="technicalSkill in technicalSkills" :key="technicalSkill.name">
+            <v-expansion-panel v-for="technicalSkill in items" :key="technicalSkill.name">
                 <v-expansion-panel-title>
                     <div class="header">
                         <div class="name">
@@ -592,7 +171,7 @@ const accumulateValue = (items: TechnicalSkillItem[], key: string): number => {
         min-height: auto;
     }
 
-    .v-table .v-table__wrapper > table > tbody > tr:not(:last-child) > td {
+    .v-table .v-table__wrapper > table > tbody > tr > td {
         padding: 10px;
     }
 }
